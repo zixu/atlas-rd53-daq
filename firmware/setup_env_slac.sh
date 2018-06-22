4e97590e4e06405ec59b5488e872228dca044037
Larry Ruckman <ruckman@slac.stanford.edu>
2018-06-22 09:55:46 -0700

    updating setup scripts for Vivado 2018.2

--- firmware/setup_env_slac.sh
+++ firmware/setup_env_slac.sh
@@ -9,4 +9,4 @@
 ##############################################################################
 
 # Setup the Xilinx Vivado and licensing
-source /afs/slac.stanford.edu/g/reseng/xilinx/vivado_2018.1/Vivado/2018.1/settings64.sh
+source /afs/slac.stanford.edu/g/reseng/xilinx/vivado_2018.2/Vivado/2018.2/settings64.sh
