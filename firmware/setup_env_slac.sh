8ed71858de5986a30592c607767242ee00b2dafd
Larry Ruckman <ruckman@slac.stanford.edu>
2018-06-22 10:04:03 -0700

    updating setup scripts

--- firmware/setup_env_slac.sh
+++ firmware/setup_env_slac.sh
@@ -10,3 +10,8 @@
 
 # Setup the Xilinx Vivado and licensing
 source /afs/slac.stanford.edu/g/reseng/xilinx/vivado_2018.2/Vivado/2018.2/settings64.sh
+
+# Firmware Simulation Software
+source /afs/slac.stanford.edu/g/reseng/synopsys/vcs-mx/M-2017.03-1/settings.sh
+source /afs/slac.stanford.edu/g/reseng/synopsys/ns/G-2012.06/settings.sh
+source /afs/slac.stanford.edu/g/reseng/synopsys/CosmosScope/D-2010.03/settings.sh
